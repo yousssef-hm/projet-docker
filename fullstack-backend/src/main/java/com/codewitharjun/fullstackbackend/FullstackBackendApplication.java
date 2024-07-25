889dc9710a20419802b7fcba7ddaee5dd0213ef2
yousssef-hm <hmy70250@gmail.com>
2024-07-25 10:56:51 +0200

    Add fullstack-backend,fullstack-frotnend

---
+++ fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/FullstackBackendApplication.java
@@ -0,0 +1,14 @@
+package com.codewitharjun.fullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ftfyt");
+		SpringApplication.run(FullstackBackendApplication.class, args);
+	}
+
+}
